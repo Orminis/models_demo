7a1e092d2414cf0806bad6ebbcc0e52c1dc0bcd5
Orminis <k.nedialkov@gmail.com>
2022-10-15 16:08:15 +0300

    registering Employee class into admin.py

--- models_demo/web/admin.py
+++ models_demo/web/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from models_demo.web.models import Employee
 
+
+# web/admin.py
 # Register your models here.
+"""
+регистрираме класа Employee тук за да може да го видим в админ панела 
+т.е. го връзва към администрацията чрез новият клас EmployeeAdmin
+и чрез декоратора admin.register.
+"""
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    pass
