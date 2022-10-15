027ea5a3a7c49cf89d67dc97a85f1f8e1c35bb26
Orminis <k.nedialkov@gmail.com>
2022-10-15 16:08:41 +0300

    Creating model Employee class

--- models_demo/web/models.py
+++ models_demo/web/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 
-
 # Create your models here.
-
-class Employee(models.Model):
-    first_name
-
-
 '''
 Employee.objects.all()      # Select *
 Employee.objects.create()   # Insert
@@ -14,3 +8,35 @@ Employee.objects.filter()   # Select + Where
 Employee.objects.update()   # Update
 Employee.objects.raw()      # raw SQL
 '''
+
+
+class Employee(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=50)
+
+    # int > 0
+    years_of_exp = models.PositiveIntegerField()
+    # Text
+    review = models.TextField()
+    # Char field with included validator for emails
+    email = models.EmailField()
+    # Boolean field (True/False)
+    works_full_time = models.BooleanField()
+    job_level = models.CharField(max_length=20)
+    photo = models.URLField()
+    # Date field
+    start_date = models.DateField()
+    # Date/Time field
+    birth_date = models.DateField()
+    # Automatically filled upon creation of an Employee
+    created_on = models.DateTimeField(auto_now=True)
+    # Automatically filled on any update of the specific employee
+    updated_on = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def fullname(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        # return f"ID: {self.pk}; Name: {self.first_name} {self.last_name}"
+        return f"ID: {self.pk}; Name: {self.fullname}"
