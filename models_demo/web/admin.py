d4e8cc7f9e1440ff70e12cc915fcc4ac0f039d3a
Orminis <k.nedialkov@gmail.com>
2022-10-17 00:39:53 +0300

    middle commit

--- models_demo/web/admin.py
+++ models_demo/web/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from models_demo.web.models import Employee
-
+from models_demo.web.models import Employee, NullBlankDemo, Department, Project
 
 # web/admin.py
 # Register your models here.
+
 """
 регистрираме класа Employee тук за да може да го видим в админ панела 
 т.е. го връзва към администрацията чрез новият клас EmployeeAdmin
@@ -11,4 +11,18 @@ from models_demo.web.models import Employee
 """
 @admin.register(Employee)
 class EmployeeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(NullBlankDemo)
+class NullBlankDemoAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
     pass
--- models_demo/web/models.py
+++ models_demo/web/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        # return f"ID: {self.pk}; Name: {self.first_name} {self.last_name}"
+        return f"ID: {self.pk}; Name: {self.name}"
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=45)
+    code_name = models.CharField(max_length=30, unique=True)
+    deadline = models.DateField()
 '''
 Employee.objects.all()      # Select *
 Employee.objects.create()   # Insert
@@ -8,12 +22,19 @@ Employee.objects.filter()   # Select + Where
 Employee.objects.update()   # Update
 Employee.objects.raw()      # raw SQL
 '''
-
-
 class Employee(models.Model):
+    LEVEL_JUNIOR = "Junior"
+    LEVEL_REGULAR = "Regular"
+    LEVEL_SENIOR = "Senior"
+
+    LEVELS = (
+        (LEVEL_JUNIOR, LEVEL_JUNIOR),
+        (LEVEL_REGULAR, LEVEL_REGULAR),
+        (LEVEL_SENIOR, LEVEL_SENIOR),
+    )
+
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=50)
-
     # int > 0
     years_of_exp = models.PositiveIntegerField()
     # Text
@@ -22,7 +43,6 @@ class Employee(models.Model):
     email = models.EmailField()
     # Boolean field (True/False)
     works_full_time = models.BooleanField()
-    job_level = models.CharField(max_length=20)
     photo = models.URLField()
     # Date field
     start_date = models.DateField()
@@ -32,6 +52,29 @@ class Employee(models.Model):
     created_on = models.DateTimeField(auto_now=True)
     # Automatically filled on any update of the specific employee
     updated_on = models.DateTimeField(auto_now_add=True)
+    # choices
+    level = models.CharField(
+        max_length=len(LEVEL_REGULAR),
+        choices=LEVELS,
+        # changes from level to Seniority Level in the admin panel. only for visualization
+        verbose_name="Seniority Level"
+    )
+
+    # one-to-many relations
+    department = models.ForeignKey(
+        to=Department,
+        # when the department is deleted all employees will be deleted too
+        on_delete=models.CASCADE,
+        default=0
+        # set null: department field for the specific employee will be set to null when his/her department is deleted
+        # on_delete=models.SET_NULL,
+        # null=True
+        # Restrict: if any employee is associated to department X, X can not be deleted because some employee is associated to it.
+        # on_delete=models.RESTRICT,
+    )
+
+    # many-to-many relations        # model to relate,
+    projects = models.ManyToManyField(Project)
 
     @property
     def fullname(self):
@@ -40,3 +83,23 @@ class Employee(models.Model):
     def __str__(self):
         # return f"ID: {self.pk}; Name: {self.first_name} {self.last_name}"
         return f"ID: {self.pk}; Name: {self.fullname}"
+
+
+# diff between null and blank
+class NullBlankDemo(models.Model):
+    # blank = models.IntegerField(
+    #     blank=True,
+    #     null=False,
+    # )
+    # null = models.IntegerField(
+    #     blank=False,
+    #     null=True,
+    # )
+    blanknull = models.IntegerField(
+        blank=True,
+        null=True,
+    )
+    default = models.IntegerField(
+        blank=False,
+        null=False,
+    )
